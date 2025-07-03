5b96ed00a3b9c08505aafc48edd60eb5f17a1bc9
Matthew Presley <matthewpresley@gmail.com>
2025-07-03 22:34:41 +0200

    Initial commit

---
+++ producer/producer.py
@@ -0,0 +1,32 @@
+from kafka import KafkaProducer
+import os
+import gzip
+import json
+import time
+
+
+def main():
+   # Assign environment variables
+    kafka_servers = os.getenv('KAFKA_BOOTSTRAP_SERVERS')
+    kafka_topic = os.getenv('TOPIC_NAME')
+
+    # Create Kafka producer object and specify the broker address 
+    producer = KafkaProducer(bootstrap_servers=kafka_servers,
+                             value_serializer=lambda v: str(v).encode('utf-8'),
+                             key_serializer=lambda v: str(v).encode('utf-8'),
+                             retries=5,
+                             acks='all')
+    
+    # Import data from Gzip file
+    with gzip.open('data/Appliances_5.json.gz', 'rb') as file:
+        for line in file:
+            record = json.loads(line)
+            print(f"Producing record: {record}")
+            # insert line to send record to Kafka broker TBC
+            producer.send(kafka_topic, value=record)
+            time.sleep(1)  # Optional: sleep to control the rate of sending messages
+
+    producer.close()
+
+if __name__ == "__main__":
+    main()
