37b8dd9cc83e8a38e1d953bb1d46809ca2b66d44
Matthew Presley <matthewpresley@gmail.com>
2025-07-07 22:18:32 +0200

    Added helper function kafka_wait to ensure broker is available for producer and consumer; Added command to copy JSON data into producer container

--- consumer/consumer.py
+++ consumer/consumer.py
@@ -1,13 +1,27 @@
 from kafka import KafkaConsumer
 import os
+import time
 
-
+def kafka_wait():
+    kafka_servers = os.getenv('KAFKA_BOOTSTRAP_SERVERS')
+    
+    # Wait for Kafka
+    start = time.time()
+    while time.time() - start < 30:
+        try:
+            admin_client = KafkaAdminClient(bootstrap_servers=kafka_servers)
+            admin_client.list_topics()
+            break
+        except Exception as e:
+            print(f"Waiting for Kafka to be ready... {e}")
 
 def main():
 
     # Assign environment variables
     kafka_servers = os.getenv('KAFKA_BOOTSTRAP_SERVERS')
     kafka_topic = os.getenv('TOPIC_NAME')
+
+    kafka_wait()  # Wait for Kafka to be ready
 
     # Create Kafa consumer object and specify the broker address
     consumer = KafkaConsumer(kafka_topic,
@@ -22,4 +36,5 @@ def main():
     consumer.close()
 
 if __name__ == "__main__":
+    #time.sleep(10)  # Optional: sleep to ensure Kafka is ready before consuming
     main()
--- producer/producer.py
+++ producer/producer.py
@@ -2,14 +2,29 @@ from kafka import KafkaProducer
 import os
 import gzip
 import json
+import time
 
-
+def kafka_wait():
+    kafka_servers = os.getenv('KAFKA_BOOTSTRAP_SERVERS')
+    
+    # Wait for Kafka
+    start = time.time()
+    while time.time() - start < 30:
+        try:
+            admin_client = KafkaAdminClient(bootstrap_servers=kafka_servers)
+            admin_client.list_topics()
+            break
+        except Exception as e:
+            print(f"Waiting for Kafka to be ready... {e}")
 
 def main():
 
    # Assign environment variables
     kafka_servers = os.getenv('KAFKA_BOOTSTRAP_SERVERS')
     kafka_topic = os.getenv('TOPIC_NAME')
+
+    # Wait for Kafka to be ready
+    kafka_wait()
 
     # Create Kafka producer object and specify the broker address 
     producer = KafkaProducer(bootstrap_servers=kafka_servers,
@@ -19,7 +34,7 @@ def main():
                              acks='all')
     
     # Import data from Gzip file
-    with gzip.open('data/Appliances_5.json.gz', 'rb') as file:
+    with gzip.open('./data/Appliances_5.json.gz', 'rb') as file:
         for line in file:
             record = json.loads(line)
             print(f"Producing record: {record}")
@@ -30,4 +45,5 @@ def main():
     producer.close()
 
 if __name__ == "__main__":
+    #time.sleep(10)  # Optional: sleep to ensure Kafka is ready before producing
     main()
